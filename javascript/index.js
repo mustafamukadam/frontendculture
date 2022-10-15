68fda64fb63824ee1aca800a9b98b2219ebf7dd4
mustafa.mukadam <mustafa.mukadam@servicenow.com>
2022-10-15 19:10:08 +0530

    added index.html and ES modules, run it with live server

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+// # async
+import "./js/async/callbacks.js";
+// import "./js/async/promises.js";
+// import "./js/async/promisesAll.js";
+// import "./js/async/promiseSetTimeOut.js";
+
+// ---------------------------------------------------------------
+
+// # closure
+// import "./js/closure/closure.js";
+// import "./js/closure/execution.js";
+
+// ---------------------------------------------------------------
+
+// # coercion
+
+// ---------------------------------------------------------------
+
+// # datatypes
+// import "./js/datatypes/datatypes.js";
--- javascript/js/async/promises.js
+++ javascript/js/async/promises.js
@@ -1,19 +1,21 @@
-function asyncResolve() {
-    function cb(resolve, reject) {
-        resolve('success')
-    }
-    return new Promise(cb);
-}
+// function asyncResolve() {
+//     function cb(resolve, reject) {
+//         resolve('success')
+//     }
+//     return new Promise(cb);
+// }
 
-function asyncReject() {
-    function cb(resolve, reject) {
-        reject('fail!')
-    }
-    return new Promise(cb);
-}
+// function asyncReject() {
+//     function cb(resolve, reject) {
+//         reject('fail!')
+//     }
+//     return new Promise(cb);
+// }
 
-let p1 = asyncResolve();
-let p2 = asyncReject();
+// let p1 = asyncResolve();
+// let p2 = asyncReject();
 
-p1.then((data) => console.log(data), (error) => console.log(error))
-p2.then((data) => console.log(data), (error) => console.log(error))
+// p1.then((data) => console.log(data), (error) => console.log(error))
+// p2.then((data) => console.log(data), (error) => console.log(error))
+
+console.log("promises...");
